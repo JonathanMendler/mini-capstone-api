78f88d7d1b20198dc365bcc66bae9fc6c7f7c094
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-10 13:11:22 -0500

    Regenerated controller with correct title. Created products in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-product = Product.new()
+product = Product.new(name: "Origin 8 Vex Platform Pedals", price: 45, image_url: "https://aventon-images.imgix.net/products/410064.jpg?auto=compress,format&q=75", description: "A lightweight, rugged pedal made from engineering-grade thermoplastic with sealed bearing and LSL sleeve bushing to take on the roughest conditions.")
+
+product = Product.new(name: "Heritage 1.0 Bike Helmet", price: 100, image_url: "https://cdn.shopify.com/s/files/1/0836/6919/products/retro_bike_helmet_001_800x.jpg?v=1611711971", description: "Designed for urban cycling, the minimalist Heritage bike helmets are inspired by the simplicity of vintage moto lids and the heritage colorways of the 50s and 60s.")
+
+product = Product.new(name: "Brooks B17 Saddle", price: 150, image_url: "https://www.petesgarage.com/images/1069-2726-large.jpg", description: "The flagship model in the brooks range. Best seller for decades!")
