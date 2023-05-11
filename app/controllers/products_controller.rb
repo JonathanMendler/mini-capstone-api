2ea4c172b424dad504f4000acec11e56accf52ac
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-11 14:13:25 -0500

    Created Update route.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,14 +18,4 @@ class ProductsController < ApplicationController
     )
     render :show
   end
-
-  # def second_product
-  #   @product = Product.second
-  #   render template: "products/show"
-  # end
-
-  # def third_product
-  #   @product = Product.third
-  #   render template: "products/show"
-  # end
 end
