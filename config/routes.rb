2ec4eccbf0331be5c4ce9fc59c2fa4fb3ed54f25
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-11 11:47:09 -0500

    Modified routes to include restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/all_products" => "products#all_products"
-  get "first_product" => "products#first_product"
-  get "second_product" => "products#second_product"
-  get "third_product" => "products#third_product"
+  get "/product/:id" => "products#show"
 end
