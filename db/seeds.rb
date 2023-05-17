02505209d485a17d9042336cc6de4ba5246ed75a
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-17 13:10:50 -0500

    Added supplier_id functional in Postico. Updated migration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 Product.create!([
-  {name: "Heritage 1.0 Bike Helmet", price: "100.0", image_url: "https://cdn.shopify.com/s/files/1/0836/6919/products/retro_bike_helmet_001_800x.jpg?v=1611711971", description: "Designed for urban cycling, the minimalist Heritage bike helmets are inspired by the simplicity of vintage moto lids and the heritage colorways of the 50s and 60s.", inventory: nil},
-  {name: "Brooks B17 Saddle", price: "150.0", image_url: "https://www.petesgarage.com/images/1069-2726-large.jpg", description: "The flagship model in the brooks range. Best seller for decades!", inventory: nil},
-  {name: "Origin 8 Vex Platform Pedals", price: "45.0", image_url: "https://aventon-images.imgix.net/products/410064.jpg?auto=compress,format&q=75", description: "A lightweight, rugged pedal made from engineering-grade thermoplastic with sealed bearing and LSL sleeve bushing to take on the roughest conditions.", inventory: nil}
+  { name: "Heritage 1.0 Bike Helmet", price: "100.0", image_url: "https://cdn.shopify.com/s/files/1/0836/6919/products/retro_bike_helmet_001_800x.jpg?v=1611711971", description: "Designed for urban cycling, the minimalist Heritage bike helmets are inspired by the simplicity of vintage moto lids and the heritage colorways of the 50s and 60s.", inventory: nil },
+  { name: "Brooks B17 Saddle", price: "150.0", image_url: "https://www.petesgarage.com/images/1069-2726-large.jpg", description: "The flagship model in the brooks range. Best seller for decades!", inventory: nil },
+  { name: "Origin 8 Vex Platform Pedals", price: "45.0", image_url: "https://aventon-images.imgix.net/products/410064.jpg?auto=compress,format&q=75", description: "A lightweight, rugged pedal made from engineering-grade thermoplastic with sealed bearing and LSL sleeve bushing to take on the roughest conditions.", inventory: nil },
 ])
+
+supplier1 = Supplier.create(name: "Thousand", email: "info@explorethousand.com", phone_number: "555-555-3876")
+supplier2 = Supplier.create(name: "Brooks", email: "BrooksEngland@email.com", phone_number: "+39 0444 46 11 00")
