75faa4bff540995fd3007cbeb4ab5beaba46d4b8
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-17 14:30:07 -0500

    Removed stupid mistake

---
+++ db/migrate/20230517192846_products_remove_url.rb
@@ -0,0 +1,5 @@
+class ProductsRemoveUrl < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :products, :url, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_190859) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_192846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_17_190859) do
     t.datetime "updated_at", null: false
     t.string "inventory"
     t.integer "supplier_id"
-    t.text "url"
   end
 
   create_table "suppliers", force: :cascade do |t|
