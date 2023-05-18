caae9b9f8829ad553496637d0c4e86e0f5cb8ea4
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-17 19:43:58 -0500

    Added Image model. Removed image_url data in Product to shift to Image model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_192846) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_003923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
