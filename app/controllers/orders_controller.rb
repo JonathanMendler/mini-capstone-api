8f9ad8939d743435fa97cf16076d56d14c0f8ad5
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-22 12:27:04 -0500

    Added index, show, & create to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def show
+    @order = Order.find_by(id: params["id"])
+    render :show
+  end
+
+  def create
+    @order = Order.create(
+      user_id: params["user_id"],
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: params["subtotal"],
+      tax: params["tax"],
+      total: params["total"],
+    )
+
+    if @order.valid?
+      render :show
+    else
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
