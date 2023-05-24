7a7231220da9c0c4b7481c40cd18e4a8e5e6a6eb
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-24 14:00:24 -0500

    Orders column removal migration

--- app/controllers/carted_product_controller.rb
+++ app/controllers/carted_product_controller.rb
@@ -1,2 +1,19 @@
 class CartedProductController < ApplicationController
+  def create
+    if current_user && current_user.admin
+      @carted_product = CartedProduct.create(
+        name: params["name"],
+        price: params["price"],
+        description: params["description"],
+        supplier_id: params["supplier_id"],
+      )
+    end
+
+    if @product.valid?
+      Image.create(product_id: @product.id, url: params["image_url"])
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
